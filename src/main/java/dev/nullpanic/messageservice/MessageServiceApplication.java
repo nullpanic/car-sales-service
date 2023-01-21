c901d888caec67f17c821ebd6a5fccdc07a3659e
gori <krash.bliz@gmail.com>
2023-01-21 18:07:52 +0300

    Created CarSalesTelegramBot Implemented interaction between TelegramApi & CarSalesTelegramBot

--- src/main/java/dev/nullpanic/messageservice/MessageServiceApplication.java
+++ src/main/java/dev/nullpanic/messageservice/MessageServiceApplication.java
@@ -1,10 +1,24 @@
 package dev.nullpanic.messageservice;
 
+import dev.nullpanic.messageservice.bot.CarSalesTelegramBot;
 import dev.nullpanic.messageservice.config.SpringConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class MessageServiceApplication {
+
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        CarSalesTelegramBot bot = context.getBean("carSalesTelegramBot", CarSalesTelegramBot.class);
+
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException exception) {
+            exception.printStackTrace();
+        }
     }
 }
--- src/main/java/dev/nullpanic/messageservice/config/SpringConfig.java
+++ src/main/java/dev/nullpanic/messageservice/config/SpringConfig.java
@@ -1,11 +1,19 @@
 package dev.nullpanic.messageservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan("dev.nullpanic.messageservice")
 @PropertySource("classpath:application.properties")
 public class SpringConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
 }
