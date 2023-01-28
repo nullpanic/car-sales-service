d5672170cefafbbdce9bef4d16eeb73c59fae013
gori <krash.bliz@gmail.com>
2023-01-28 15:01:19 +0300

    Add spring mvc

---
+++ src/main/java/dev/nullpanic/messageservice/config/WebConfig.java
@@ -0,0 +1,12 @@
+package dev.nullpanic.messageservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"dev.nullpanic.messageservice"})
+public class WebConfig {
+
+}
